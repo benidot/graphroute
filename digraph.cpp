26a6f627adc72f892962bb68ca6a134dbc1139bb
AlanDeMellow <alan.g.m@outlook.com>
2026-06-17 19:51:17 -0300

    Formatação do código

--- digraph.cpp
+++ digraph.cpp
@@ -12,181 +12,162 @@ namespace dg {
 
 template <typename T>
 class digraph {
-private:
-    struct node {
-        T value;
-        std::unordered_set<node*> links;
-    };
+ private:
+  struct node {
+    T value;
+    std::unordered_set<node*> links;
+  };
 
-    std::unordered_map<T, node> graph;
+  std::unordered_map<T, node> graph;
 
-    node* find(const T& val)
-    {
-        auto it = graph.find(val);
-        if (it == graph.end()) { // Not found!
-            return nullptr;
+  node* find(const T& val) {
+    auto it = graph.find(val);
+    if (it == graph.end()) {  // Not found!
+      return nullptr;
+    }
+    return &it->second;
+  }
+
+  void visit_node(node* p, std::unordered_set<node*>& visited, int level) {
+    std::cout << std::string(level, '\t') << p->value << "\n";
+    visited.insert(p);
+    for (auto adj : p->links) {
+      if (visited.count(adj) == 0) visit_node(adj, visited, level + 1);
+    }
+  }
+
+ public:
+  void insert_node(const T& val) {
+    if (val == "*") return;
+    if (graph.count(val) != 0) return;
+    node aux;
+    aux.value = val;
+    graph[val] = aux;
+  }
+
+  void insert_link(const T& from, const T& to) {
+    auto pfrom = find(from);
+    if (!pfrom) return;
+    auto pto = find(to);
+    if (!pto || pto->value == "*") return;
+
+    pfrom->links.insert(pto);
+  }
+
+  void remove(const T& val) {
+    auto p = find(val);
+    if (!p) return;
+    for (auto& [key, node] : graph) {
+      node.links.erase(p);
+    }
+    graph.erase(val);
+  }
+
+  void show(std::string& choice) {
+    std::ofstream dot("g123456.dot");
+    dot << "digraph{\n";
+    for (const auto& [key, node] : graph) {
+      dot << "\t\"" << key << "\" -> {";
+      for (const auto& link : node.links) {
+        dot << "\"" << link->value << "\" ";
+      }
+      dot << "};\n";
+    }
+    dot << "}\n";
+    dot.close();
+    std::string final = "";
+    if (choice == "-Tpng") {
+      final = " -o out.png";
+    } else if (choice == "-Tpdf") {
+      final = " -o out.pdf";
+    }
+    std::string call = "dot " + choice + " g123456.dot" + final;
+    system(call.c_str());
+  }
+
+  void DFS_from(const T& from) {
+    auto p = find(from);
+    if (!p) return;
+
+    std::unordered_set<node*> visited;
+    visit_node(p, visited, 0);
+  }
+
+  std::vector<node*> shortest_path(const T& start, const T& end) {
+    std::vector<node*> path;
+
+    auto pstart = find(start);
+    if (!pstart) return path;
+
+    auto pend = find(end);
+    if (!pend) return path;
+
+    std::queue<node*> q;
+    std::unordered_set<node*> queued;
+    std::unordered_map<node*, node*> origin;
+    q.push(pstart);
+    queued.insert(pstart);
+    origin[pstart] = nullptr;
+    bool found = false;
+
+    while (!q.empty()) {
+      auto current = q.front();
+      q.pop();
+      if (current == pend) {
+        found = true;
+        break;
+      }
+      for (auto adj : current->links) {
+        if (queued.count(adj) == 0) {
+          q.push(adj);
+          queued.insert(adj);
+          origin[adj] = current;
         }
-        return &it->second;
+      }
+    }
+    if (found) {
+      auto p = pend;
+      while (p) {
+        path.push_back(p);
+        p = origin[p];
+      }
+    }
+    std::reverse(path.begin(), path.end());
+
+    return path;
+  }
+
+  void load(const std::string& filename) {
+    std::ifstream in(filename);
+    if (!in) return;
+    int n;
+    in >> n;
+    in.ignore();
+    std::string line, line2;
+    while (n--) {
+      getline(in, line);
+      insert_node(line);
+    }
+    while (getline(in, line) && getline(in, line2)) {
+      insert_link(line, line2);
     }
 
-    void visit_node(node* p, std::unordered_set<node*>& visited, int level)
-    {
-        std::cout << std::string(level, '\t') << p->value << "\n";
-        visited.insert(p);
-        for (auto adj : p->links) {
-            if (visited.count(adj) == 0)
-                visit_node(adj, visited, level + 1);
-        }
+    in.close();
+  }
+
+  void save(const std::string& filename) {
+    std::ofstream out(filename);
+    out << graph.size() << "\n";
+    for (const auto& [key, node] : graph) {
+      out << node.value << "\n";
     }
-
-public:
-    void insert_node(const T& val)
-    {
-        if(val == "*") return;
-        if (graph.count(val) != 0)
-            return;
-        node aux;
-        aux.value = val;
-        graph[val] = aux;
+    for (const auto& [key, node] : graph) {
+      for (const auto& link : node.links) {
+        out << key << '\n';
+        out << link->value << '\n';
+      }
     }
-
-    void insert_link(const T& from, const T& to)
-    {
-        auto pfrom = find(from);
-        if (!pfrom)
-            return;
-        auto pto = find(to);
-        if (!pto || pto->value == "*")
-            return;
-
-        pfrom->links.insert(pto);
-    }
-
-    void remove(const T& val)
-    {
-        auto p = find(val);
-        if (!p)
-            return;
-        for (auto& [key, node] : graph) {
-            node.links.erase(p);
-        }
-        graph.erase(val);
-    }
-
-    void show(std::string &choice)
-    {
-        std::ofstream dot("g123456.dot");
-        dot << "digraph{\n";
-        for (const auto& [key, node] : graph) {
-            dot << "\t\"" << key << "\" -> {";
-            for (const auto& link : node.links) {
-                dot << "\"" << link->value << "\" ";
-            }
-            dot << "};\n";
-        }
-        dot << "}\n";
-        dot.close();
-        std::string final = "";
-        if(choice == "-Tpng") {
-            final = " -o out.png";
-        } else if (choice == "-Tpdf") {
-            final = " -o out.pdf";
-        }
-        std::string call = "dot " + choice + " g123456.dot" + final;
-        system(call.c_str());
-    }
-
-    void DFS_from(const T& from)
-    {
-        auto p = find(from);
-        if (!p)
-            return;
-
-        std::unordered_set<node*> visited;
-        visit_node(p, visited, 0);
-    }
-
-    std::vector<node *> shortest_path(const T& start, const T& end)
-    {
-        std::vector<node *> path;
-
-        auto pstart = find(start);
-        if (!pstart)
-            return path;
-
-        auto pend = find(end);
-        if (!pend)
-            return path;
-
-        std::queue<node*> q;
-        std::unordered_set<node*> queued;
-        std::unordered_map<node*, node*> origin;
-        q.push(pstart);
-        queued.insert(pstart);
-        origin[pstart] = nullptr;
-        bool found = false;
-
-        while (!q.empty()) {
-            auto current = q.front();
-            q.pop();
-            if (current == pend){
-                found = true;
-                break;
-            }
-            for (auto adj : current->links) {
-                if (queued.count(adj) == 0) {
-                    q.push(adj);
-                    queued.insert(adj);
-                    origin[adj] = current;
-                }
-            }            
-        }
-        if(found) {
-            auto p = pend;
-            while(p){
-                path.push_back(p);
-                p = origin[p];
-            }
-        }
-        std::reverse(path.begin(), path.end());
-
-        return path;
-    }
-
-    void load(const std::string& filename)
-    {
-        std::ifstream in(filename);
-        if (!in)
-            return;
-        int n;
-        in >> n;
-        in.ignore();
-        std::string line, line2;
-        while (n--) {
-            getline(in, line);
-            insert_node(line);
-        }
-        while (getline(in, line) && getline(in, line2)) {
-            insert_link(line, line2);
-        }
-
-        in.close();
-    }
-
-    void save(const std::string& filename)
-    {
-        std::ofstream out(filename);
-        out << graph.size() << "\n";
-        for (const auto& [key, node] : graph) {
-            out << node.value << "\n";
-        }
-        for (const auto& [key, node] : graph) {
-            for (const auto& link : node.links) {
-                out << key << '\n';
-                out << link->value << '\n';
-            }
-        }
-        out.close();
-    }
+    out.close();
+  }
 };
-}
+}  // namespace dg
--- graphroute.cpp
+++ graphroute.cpp
@@ -1,98 +1,105 @@
-#include <iostream>
 #include <fstream>
-#include <vector>
-#include <string>
+#include <iostream>
 #include <sstream>
+#include <string>
+#include <vector>
+
 #include "digraph.cpp"
 
 using namespace std;
 
 struct graph {
-    vector<string> headers;
-    vector<vector<string>> rows;
+  vector<string> headers;
+  vector<vector<string>> rows;
 };
 
 string sel_saida() {
-    cout << "Selecione o formato de saida do GraphViz:\n";
-    cout << "1. Tela\n2. PNG\n3. PDF\nOpcao: ";
-    int c; cin >> c;
-    switch (c) {
-        case 1:
-            return "-Tx11";
-            break;
-        case 2:
-            return "-Tpng";
-            break;
-        case 3:
-            return "-Tpdf";
-            break;
-        default:
-            return "erro";
-            break;
+  cout << "Selecione o formato de saida do GraphViz:\n";
+  cout << "1. Tela\n2. PNG\n3. PDF\nOpcao: ";
+  int c;
+  cin >> c;
+  switch (c) {
+    case 1:
+      return "-Tx11";
+      break;
+    case 2:
+      return "-Tpng";
+      break;
+    case 3:
+      return "-Tpdf";
+      break;
+    default:
+      return "erro";
+      break;
+  }
+}
+
+void csv(ifstream& inp, vector<string>& headers, vector<vector<string>>& rows) {
+  string line;
+  if (getline(inp, line)) {
+    stringstream ss(line);
+    string header;
+    while (getline(ss, header, ',')) {
+      headers.push_back(header);
     }
+  }  // primeira linha
+
+  while (getline(inp, line)) {
+    stringstream ss(line);
+    string cell;
+    vector<string> row;
+    while (getline(ss, cell, ',')) {
+      row.push_back(cell);
+    }
+    rows.push_back(row);
+  }  // resto
 }
 
-void csv(ifstream &inp, vector<string> &headers, vector<vector<string>> &rows) {
-    string line;
-    if(getline(inp, line)) {
-        stringstream ss(line);
-        string header;
-        while(getline(ss, header, ',')) {
-            headers.push_back(header);
-        }
-    } // primeira linha
-
-    while(getline(inp, line)) {
-        stringstream ss(line);
-        string cell;
-        vector<string> row;
-        while(getline(ss, cell, ',')) {
-            row.push_back(cell);
-        }
-        rows.push_back(row);
-    } // resto
-}
-
-void make_graph (ifstream &inp, vector<string> &headers, vector<vector<string>> &rows, dg::digraph<string> &root) {
-
-    for (const auto& row : rows) {
-        for (const auto& cell : row) {
-            if (cell == "*" || cell == "") {
-                continue;
-            }
-        }
-
-        root.insert_node(row[4]);
-        root.insert_node(row[5]);
-        root.insert_link(row[4], row[5]);
-
+void make_graph(ifstream& inp, vector<string>& headers,
+                vector<vector<string>>& rows, dg::digraph<string>& root) {
+  for (const auto& row : rows) {
+    for (const auto& cell : row) {
+      if (cell == "*" || cell == "") {
+        continue;
+      }
     }
 
+    root.insert_node(row[4]);
+    root.insert_node(row[5]);
+    root.insert_link(row[4], row[5]);
+  }
 }
 
-void fullprint(const graph &A) {
-    for (const auto& header : A.headers) {
-        cout << header << "\t";
-    } cout << endl;
+void fullprint(const graph& A) {
+  for (const auto& header : A.headers) {
+    cout << header << "\t";
+  }
+  cout << endl;
 
-    for (const auto& row : A.rows) {
-        for (const auto& cell : row) {
-            cout << cell << "\t";
-        }
-        cout << endl;
-    } // so pra printar por agora
+  for (const auto& row : A.rows) {
+    for (const auto& cell : row) {
+      cout << cell << "\t";
+    }
+    cout << endl;
+  }  // so pra printar por agora
 }
 
-int main(int argc, char *argv[]) {
-    ifstream inp; graph A; dg::digraph<string> root;
-    inp.open(argv[1]);
-    if(!inp) {cout << "Erro: Arquivo Invalido\n"; return -1;};
-    cout << "Arquivo valido. Continuando.\n";
-    csv(inp, A.headers, A.rows);
-    make_graph(inp, A.headers, A.rows, root);
-    string choice = sel_saida();
-    root.show(choice);
-    //fullprint();
+int main(int argc, char* argv[]) {
+  ifstream inp;
+  graph A;
+  dg::digraph<string> root;
+  inp.open(argv[1]);
 
-    inp.close();
+  if (!inp) {
+    cout << "Erro: Arquivo Invalido\n";
+    return -1;
+  };
+  cout << "Arquivo valido. Continuando.\n";
+  csv(inp, A.headers, A.rows);
+  make_graph(inp, A.headers, A.rows, root);
+  string choice = sel_saida();
+  root.show(choice);
+  // fullprint();
+
+  inp.close();
 }
